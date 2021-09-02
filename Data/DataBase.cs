7694b79012a47441d6b2341756c758fd789b1eff
greSvoi <accc32@gmail.com>
2021-09-02 15:33:57 +0300

    Добавьте файлы проекта.

---
+++ Data/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidationApp.Data
+{
+    public class DataBase:DbContext
+    {
+        public DataBase(DbContextOptions<DataBase> options) : base(options)
+        {
+
+        }
+    }
+}
